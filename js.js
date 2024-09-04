e8d02c06fc47d4f1c88f0d517aa053824e6ff05b
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-09-04 14:19:09 +0300

    ll

---
+++ js.js
@@ -0,0 +1,43 @@
+const age = "14"
+ console.log(age);
+
+
+ const name = "kirill"
+ console.log(name);
+
+ const isStudent = "true"
+
+ console.log(isStudent);
+
+ const myString  = "упорство і труд газопровід вкрадут"
+
+ console.log(myString);
+
+ const mynumber = "10"
+
+ console.log(mynumber);
+
+ const Null = "null"
+
+ console.log(null);
+
+ prompt ()
+
+ confirm ()
+
+ alert ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
